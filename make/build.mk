fa0cf10ebc9de2f16ab67c37ea69686227bded72
wanglei4687 <wanglei4687@gmail.com>
2023-09-29 15:07:54 +0800

    update
    Signed-off-by: wanglei4687 <wanglei4687@gmail.com>

--- make/build.mk
+++ make/build.mk
@@ -1,4 +1,18 @@
-GOBUILD_FLAG = $()
+GOOS?=$(shell go env GOOS)
+GOARCH?=$(shell go env GOARCH)
+ifeq ($(GOARCH),arm)
+	ARCH=armv7
+else
+	ARCH=$(GOARCH)
+endif
+
+
+GO_BUILD_RECIPE=\
+	GOOS=$(GOOS) \
+	GOARCH=$(GOARCH) \
+	CGO_ENABLED=0 \
+	go build
 
 build: 
 	$(info Build...)
+	$(GO_BUILD_RECIPE) -o bin/main cmd/main/main.go
